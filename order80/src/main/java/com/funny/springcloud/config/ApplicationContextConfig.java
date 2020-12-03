10a8865148086ad35627f92aa827d6487d26c73f
funny-shan <13415663598@163.com>
2020-12-03 09:55:15 +0800

    order Project commit

---
+++ order80/src/main/java/com/funny/springcloud/config/ApplicationContextConfig.java
@@ -0,0 +1,13 @@
+package com.funny.springcloud.config;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootConfiguration
+public class ApplicationContextConfig {
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+}
---
+++ order80/src/main/java/com/funny/springcloud/controller/OrderController.java
@@ -0,0 +1,32 @@
+package com.funny.springcloud.controller;
+
+import com.funny.springcloud.entities.Payment;
+import com.funny.springcloud.entities.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderController {
+    public static final String PAYMENT_URL = "http://localhost:8001";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping("/consumer/payment/save")
+    public Result<Payment> create(Payment payment){
+        log.info("保存数据，参数：{}",payment);
+        return restTemplate.postForObject(PAYMENT_URL+"/payment/save",payment,Result.class);  //写操作
+    }
+
+    @GetMapping("/consumer/payment/get/{id}")
+    public Result<Payment> getPayment(@PathVariable("id") Long id){
+        return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,Result.class);
+    }
+}
+
