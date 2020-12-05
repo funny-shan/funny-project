dbb8d5974151d13ca37c1dc3bb30b0a29b61419c
funny-shan <13415663598@163.com>
2020-12-05 17:50:10 +0800

    负载均衡

--- order80/src/main/java/com/funny/springcloud/config/ApplicationContextConfig.java
+++ order80/src/main/java/com/funny/springcloud/config/ApplicationContextConfig.java
@@ -1,12 +1,14 @@
 package com.funny.springcloud.config;
 
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootConfiguration
 public class ApplicationContextConfig {
     @Bean
+    @LoadBalanced
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
     }
