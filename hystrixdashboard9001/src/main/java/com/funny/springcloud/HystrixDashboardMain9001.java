79ba6e90177882ac1cf14db6a127100c18d894a2
funny-shan <13415663598@163.com>
2020-12-20 00:06:59 +0800

    HystrixDashboard服务监控案例

---
+++ hystrixdashboard9001/src/main/java/com/funny/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,18 @@
+package com.funny.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @Author linshan
+ * @Date 2020/12/19
+ * @desc
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class,args);
+    }
+}
