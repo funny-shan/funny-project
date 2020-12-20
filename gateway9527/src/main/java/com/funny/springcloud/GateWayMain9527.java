9f2182748cadb40f26753f3ebac2b49bdb8be76e
funny-shan <13415663598@163.com>
2020-12-20 18:05:55 +0800

    gateway简单网关路由配置案例

---
+++ gateway9527/src/main/java/com/funny/springcloud/GateWayMain9527.java
@@ -0,0 +1,18 @@
+package com.funny.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author linshan
+ * @Date 2020/12/20
+ * @desc
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run( GateWayMain9527.class,args);
+    }
+}
