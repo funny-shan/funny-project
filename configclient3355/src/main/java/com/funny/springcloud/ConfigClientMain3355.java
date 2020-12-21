4970ec043d450a3c885273abec43e9914a83959c
linshan <13415663598@163.com>
2020-12-21 22:07:13 +0800

    config配置中心客户端搭建案例 半自动刷新配置

---
+++ configclient3355/src/main/java/com/funny/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,16 @@
+package com.funny.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author linshan
+ * @Date 2020/12/21
+ * @desc
+ */
+@SpringBootApplication
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run( ConfigClientMain3355.class,args);
+    }
+}
