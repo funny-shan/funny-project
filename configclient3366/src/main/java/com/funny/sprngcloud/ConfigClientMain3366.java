d1a7802bb34f05ac300e6fe3c8fbeb6fd49f3c1b
linshan <13415663598@163.com>
2020-12-23 23:42:10 +0800

    bus消息总线实现全局通知案例

---
+++ configclient3366/src/main/java/com/funny/sprngcloud/ConfigClientMain3366.java
@@ -0,0 +1,16 @@
+package com.funny.sprngcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author linshan
+ * @Date 2020/12/23
+ * @desc
+ */
+@SpringBootApplication
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run( ConfigClientMain3366.class,args);
+    }
+}
