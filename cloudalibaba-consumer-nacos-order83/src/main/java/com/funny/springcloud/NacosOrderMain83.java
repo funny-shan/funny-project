2360f4f5e23ccef3710e5c591c9287ef406f8eb2
linshan <linshan>
2021-01-21 15:09:38 +0800

    SpringCloud Alibaba Nacos作为服务注册中心使用，自带负载均衡

---
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/funny/springcloud/NacosOrderMain83.java
@@ -0,0 +1,13 @@
+package com.funny.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosOrderMain83 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosOrderMain83.class,args);
+    }
+}
---
+++ cloudalibaba-provider-payment9001/src/main/java/com/funny/springcloud/AlibabaPaymentMain9001.java
@@ -0,0 +1,14 @@
+package com.funny.springcloud;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class AlibabaPaymentMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(AlibabaPaymentMain9001.class,args);
+    }
+}
